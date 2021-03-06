b41f5a18a4178471c2f69c96b396f35a89b358bd
Sahil Garg <70465328+SaGaVoices@users.noreply.github.com>
2021-03-07 00:27:03 +0530

    Update shell.c

--- Creating_Shell_in_C/shell.c
+++ Creating_Shell_in_C/shell.c
@@ -20,6 +20,7 @@
 #include<readline/readline.h> 
 #include<readline/history.h> 
 #include <limits.h>
+#include <dirent.h>
   
 #define MAXCOM 1000 // max number of letters to be supported 
 #define MAXLIST 1000 // max number of commands to be supported 
@@ -27,12 +28,7 @@
 // Clearing the shell using escape sequences 
 #define clr() printf("\033[H\033[J") 
 
-//char *cmdsHistory[100];
-//int cmdHis = 0;
 
-
-
-  
 // Greeting shell during startup 
 void init_shell() 
 { 
@@ -55,11 +51,9 @@ void add_History(char* str)
 {
 	FILE * fptr;
 	fptr = fopen("history.txt", "a+");
-    	//cmdsHistory[cmdHis] = str;
     	fputs(str, fptr);
     	fputs("\n", fptr);
     	fclose(fptr);
-    	//cmdHis++;
 }
   
 // Function to take input 
@@ -68,8 +62,11 @@ int takeInput(char* str)
     char* buf; 
   
     buf = readline(">>> "); 
+    if(buf == 0){
+    printf("\n\nBye Bye!!\nHave a nice day!\n\n");
+    exit(0);
+    }
     if (strlen(buf) != 0) { 
-        add_history(buf); 
         strcpy(str, buf);
         add_History(buf);  
         return 0; 
@@ -78,31 +75,11 @@ int takeInput(char* str)
     } 
 } 
 
-/**int takeRecInput(char* str, char * line)
-{
-	char * buf;
-	
-	buf = line;
-	if (strlen(buf) != 0) { 
-        	add_history(buf); 
-        	strcpy(str, buf);
-        	add_History(buf); 
-        	printf("%s", str); 
-        	return 0; 
-    	} else{
-        	return 1; 
-    	} 
-	
-}**/
-
-
 //Function to pause
 void pauseStop(){
-while (getchar() != '\n') ;
+    while (getchar() != '\n') ;
 }
 
- 
- 
 // Function to print Current Directory. 
 void printDir() 
 { 
@@ -110,108 +87,35 @@ void printDir()
     getcwd(cwd, sizeof(cwd)); 
     printf("\nshell=<%s>=myshell", cwd); 
 } 
-  
-// Function where the system command is executed 
-void execArgs(char** parsed) 
-{ 
-    // Forking a child 
-    int i, j;
-    i = 0;
-    j = -1;
-    pid_t pid = fork();  
-  
-    if (pid == j) { 
-        printf("\nFailed forking child.."); 
-        return; 
-    } else if (pid == i) { 
-        if (execvp(parsed[i], parsed) < i) { 
-            printf("\nCould not execute command.."); 
-        } 
-        exit(0); 
-    } else { 
-        // waiting for child to terminate 
-        wait(NULL);  
-        return; 
-    } 
-} 
-  
-// Function where the piped system commands is executed 
-void execArgsPiped(char** parsed, char** parsedpipe) 
-{ 
-    // 0 is read end, 1 is write end 
-    int pipefd[2];  
-    pid_t p1, p2; 
-  
-    if (pipe(pipefd) < 0) { 
-        printf("\nPipe could not be initialized"); 
-        return; 
-    } 
-    p1 = fork(); 
-    if (p1 < 0) { 
-        printf("\nCould not fork"); 
-        return; 
-    } 
-  
-    if (p1 == 0) { 
-        // Child 1 executing.. 
-        // It only needs to write at the write end 
-        close(pipefd[0]); 
-        dup2(pipefd[1], STDOUT_FILENO); 
-        close(pipefd[1]); 
-  
-        if (execvp(parsed[0], parsed) < 0) { 
-            printf("\nCould not execute command 1.."); 
-            exit(0); 
-        } 
-    } else { 
-        // Parent executing 
-        p2 = fork(); 
-  
-        if (p2 < 0) { 
-            printf("\nCould not fork"); 
-            return; 
-        } 
-  
-        // Child 2 executing.. 
-        // It only needs to read at the read end 
-        if (p2 == 0) { 
-            close(pipefd[1]); 
-            dup2(pipefd[0], STDIN_FILENO); 
-            close(pipefd[0]); 
-            if (execvp(parsedpipe[0], parsedpipe) < 0) { 
-                printf("\nCould not execute command 2.."); 
-                exit(0); 
-            } 
-        } else { 
-            // parent executing, waiting for two children 
-            wait(NULL); 
-            wait(NULL); 
-        } 
-    } 
-} 
 
-
+//Printing a string without multiple spaces in between
+void print_without_spaces(char* str)
+{
+    while(*str && *str == ' ')
+        str++;
+    while(*str)
+    {
+        if(*(str) != '\0' && (*(str) != ' ' || (*(str-1) != ' ' && *(str) == ' ')))
+            putchar(*str);
+        str++;
+    }
+    printf("\n");
+}
+  
 // History Writing in Shell
 void writeHistory()
 {
         
-        FILE* file = fopen("history.txt", "r"); /* should check the result */
+        FILE* file = fopen("history.txt", "r"); 
     	char line[256];
     	
     	int i = 0;
 
    	while (fgets(line, sizeof(line), file)) {
-        /* note that fgets don't strip the terminating \n, checking its
-           presence would allow to handle lines longer that sizeof(line) */
         	printf("History Command %d: %s", i, line);
         	i++; 
     	}
-    	/* may check feof here to make a difference between eof and io failure -- network
-       	timeout for instance */
-
-    	fclose(file);
-        
-        
+	fclose(file);
         return;
 }
   
@@ -232,13 +136,34 @@ void openHelp()
   
     return; 
 } 
+
+//Function to get all the elements in a given directory
+void getdirectory (char *path)
+{
+  DIR *dp;
+  struct dirent *ep;     
+  dp = opendir (path);
+
+  if (dp != NULL)
+  {
+    while ((ep = readdir(dp)) != NULL) 
+    if((strcmp(ep->d_name, ".") != 0) && (strcmp(ep->d_name, "..") != 0)){
+        printf("%s\n", ep->d_name);}
+
+    closedir (dp);
+  }
+  else
+    perror ("Couldn't open the directory");
+}
+
   
 // Function to execute builtin commands 
 int ownCmdHandler(char** parsed) 
 { 
-    int NoOfOwnCmds = 6, i, switchOwnArg = 0; 
+    int NoOfOwnCmds = 8, i, switchOwnArg = 0; 
     char* ListOfOwnCmds[NoOfOwnCmds]; 
     char* username; 
+    int tem_ind = 1;
   
     ListOfOwnCmds[0] = "quit"; 
     ListOfOwnCmds[1] = "cd"; 
@@ -246,6 +171,8 @@ int ownCmdHandler(char** parsed)
     ListOfOwnCmds[3] = "clr";
     ListOfOwnCmds[4] = "history";
     ListOfOwnCmds[5] = "pause";
+    ListOfOwnCmds[6] = "echo";
+    ListOfOwnCmds[7] = "dir";
   
     for (i = 0; i < NoOfOwnCmds; i++) { 
         if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) { 
@@ -256,10 +183,17 @@ int ownCmdHandler(char** parsed)
   
     switch (switchOwnArg) { 
     case 1: 
-        printf("\nBye Bye!!\nHave a nice day!!\n"); 
+        printf("\nBye Bye!!\nHave a nice day!!\n\n"); 
         exit(0); 
     case 2: 
-        chdir(parsed[1]); 
+    	if(parsed[1] == NULL){
+    		printf("No path given\n");
+    	}
+    	else{
+        	if(chdir(parsed[1]) == -1){
+        		perror("Dir doesn't exist!");
+        	}
+        } 
         return 1; 
     case 3: 
         openHelp(); 
@@ -273,6 +207,21 @@ int ownCmdHandler(char** parsed)
     case 6:
     	pauseStop();
     	return 1;
+    case 7:
+        while(parsed[tem_ind]!=NULL) {
+             printf("%s ", parsed[tem_ind]);
+             tem_ind++;
+        }
+        printf("\n");
+        return 1;
+    case 8:
+	if (parsed[1] != NULL){
+               getdirectory(parsed[1]);
+        }
+        else {
+            printf("No directory specified\n");
+        }
+        return 0;
     default: 
         break; 
     } 
@@ -280,22 +229,6 @@ int ownCmdHandler(char** parsed)
     return 0; 
 } 
   
-// function for finding pipe 
-int parsePipe(char* str, char** strpiped) 
-{ 
-    int i; 
-    for (i = 0; i < 2; i++) { 
-        strpiped[i] = strsep(&str, "|"); 
-        if (strpiped[i] == NULL) 
-            break; 
-    } 
-  
-    if (strpiped[1] == NULL) 
-        return 0; // returns zero if no pipe is found. 
-    else { 
-        return 1; 
-    } 
-} 
   
 // function for parsing command words 
 void parseSpace(char* str, char** parsed) 
@@ -312,37 +245,17 @@ void parseSpace(char* str, char** parsed)
     } 
 } 
   
-int processString(char* str, char** parsed, char** parsedpipe) 
-{ 
-  
-    char* strpiped[2]; 
-    int piped = 0; 
-    int i, j, k;
-    i = 0;
-    j = 1;
-  
-    piped = parsePipe(str, strpiped); 
-  
-    if (piped) { 
-        parseSpace(strpiped[i], parsed); 
-        parseSpace(strpiped[j], parsedpipe); 
-  
-    } else { 
-  
-        parseSpace(str, parsed); 
-    } 
-  
-    if (ownCmdHandler(parsed)) 
-        return 0; 
-    else
-        return 1 + piped; 
+void processString(char* str, char** parsed) 
+{  
+    parseSpace(str, parsed); 
+    ownCmdHandler(parsed); 
+    return;    
 } 
-  
+
+
 int main(int argc, char *argv[], char * envp[]) 
 { 
     char inputString[MAXCOM], *parsedArgs[MAXLIST]; 
-    char* parsedArgsPiped[MAXLIST]; 
-    int execFlag = 0; 
     char * string;
     char * first;
     char * second;
@@ -353,8 +266,7 @@ int main(int argc, char *argv[], char * envp[])
     while (1) { 
         // print shell line 
         printDir(); 
-        
-        
+       
         // take input 
         if (takeInput(inputString)) 
             continue; 
@@ -370,33 +282,26 @@ int main(int argc, char *argv[], char * envp[])
     		{    
         		printf("\n%s", envp[i]);
     		}
+    		printf("\n");
 	}
 	
 	//Reading the first string of input command
 	first = strsep(&string," ");
 	
-	
 	//Checking if the first string is myshell and if yes, then executing the commands written in batchfile.txt
 	if(strcmp(first , "myshell") == 0){
 		second = strsep(&string, " ");
 		
-		//printf("%s", second);
-		
 		FILE* file = fopen(second, "r"); /* should check the result */
     		char line[256];
-    	
-    		char inputString_[MAXCOM], *parsedArgs_[MAXLIST]; 
-    		char* parsedArgsPiped_[MAXLIST];
     		int execFlag_ = 0; 
 
    		while (fgets(line, sizeof(line), file)) {
-        		// process 
-        		//printf("History Command: %s", line);
-        		//line = line.resize(line.size() - 1);
+        		
         		int size = strlen(line); //Total size of string
 			line[size-1] = '\0';
-        		//if(takeRecInput(inputString_, line))
-        		//	break;
+			char inputString[MAXCOM];
+			strcpy(inputString, line);
         		printf("\n");
         		if(strcmp(line, "environ") == 0)
         		{
@@ -405,43 +310,22 @@ int main(int argc, char *argv[], char * envp[])
     				{    
         				printf("\n%s", envp[i]);
     				}
+    				printf("\n");
 			}
 			
-			execFlag = processString(line, 
-        		parsedArgs, parsedArgsPiped); 
-        		// execflag returns zero if there is no command 
-        		// or it is a builtin command, 
-        		// 1 if it is a simple command 
-        		// 2 if it is including a pipe. 
+			processString(inputString, 
+        		parsedArgs); 
   
-        		// execute 
-        		if (execFlag == 1) 
-            			execArgs(parsedArgs); 
-  
- 		       if (execFlag == 2) 
- 		           execArgsPiped(parsedArgs, parsedArgsPiped); 
  	   	}
 
     		fclose(file);
     	
 		
 	}else {
-	
-	
-        
-        execFlag = processString(inputString, 
-        parsedArgs, parsedArgsPiped); 
-        // execflag returns zero if there is no command 
-        // or it is a builtin command, 
-        // 1 if it is a simple command 
-        // 2 if it is including a pipe. 
+		processString(inputString, 
+        	parsedArgs);  
   
-        // execute 
-        if (execFlag == 1) 
-            execArgs(parsedArgs); 
-  
-        if (execFlag == 2) 
-            execArgsPiped(parsedArgs, parsedArgsPiped); }
+   	}
     } 
     return 0; 
 } 
